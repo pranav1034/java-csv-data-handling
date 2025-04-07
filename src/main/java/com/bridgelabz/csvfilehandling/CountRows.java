e8e8249b18df4a5dd62ac8cd03c943f93fcdef39
pranav1034 <pranavaggarwal04@gmail.com>
2025-04-07 15:34:13 +0530

    [Pranav] ADD read-and-count-number-of-rows-in-csv-file

---
+++ src/main/java/com/bridgelabz/csvfilehandling/CountRows.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.csvfilehandling;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class CountRows {
+    public static void main(String[] args) {
+        String path = "Students.csv";
+        String line;
+        int count=0;
+        try{
+            BufferedReader br=new BufferedReader(new FileReader(path));
+            String head= br.readLine();
+            while((line=br.readLine())!=null){
+                count++;
+            }
+            System.out.println(count);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
+
