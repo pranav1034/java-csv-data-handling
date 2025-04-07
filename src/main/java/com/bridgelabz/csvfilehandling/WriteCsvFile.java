d3b036f920fb2d1d8e29978c7e28ebbb42abb05d
pranav1034 <pranavaggarwal04@gmail.com>
2025-04-07 15:23:37 +0530

    [Pranav] ADD write-data-to-a-csv-file

---
+++ src/main/java/com/bridgelabz/csvfilehandling/WriteCsvFile.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.csvfilehandling;
+
+import java.io.FileWriter;
+
+public class WriteCsvFile {
+    public static void main(String[] args) {
+        String path = "Students2.csv";
+        try{
+            FileWriter writer = new FileWriter(path);
+            writer.append("ID,Name,Department,Salary\n");
+
+            writer.append("1,Pranav,Engineering,95000\n");
+            writer.append("2,Alan,Marketing,88000\n");
+            writer.append("3,Aman,Finance,72000\n");
+            writer.append("4,Nikhil,Human Resources,65000\n");
+            writer.append("5,David,Engineering,80000\n");
+
+            writer.flush();
+            writer.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
